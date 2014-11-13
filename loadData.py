0b007db6646a424e4b51ebd83935f963628b716c
devintjones <devin.thatcher.jones@gmail.com>
2014-11-12 22:35:11 -0500

    function to chunk input and inset

--- loadData.py
+++ loadData.py
@@ -1,51 +1,68 @@
-import py2neo
-from py2neo import neo4j
+from py2neo import neo4j, node, rel
 import csv
-
-node = py2neo.node
-rel  = py2neo.rel
-
-rawData = []
-
-users = []
-songs = []
-forNeo = []
-with open("/home/devinj/neo4jData/ydata-ymusic-rating-study-v1_0-test.txt",'rb') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter='\t')
-    for row in csvreader:
-        rawData.append(row)
-        if row[0] not in users:
-            users.append(row[0])
-            currentUser = node(uid=row[0])
-            #forNeo.append(currentUser)
-        if row[1] not in songs:
-            songs.append(row[1])
-            currentSong = node(songid=row[1])
-            #forNeo.append(currentSong)
-        forNeo.append(rel(currentUser,("RATED",{"rating": row[2]}),currentSong))
 
 
 def head(data,i=9):
     for row in range(0,i):
         print data[row]
+def tail(data,i=9):
+    for row in range(len(data)-i,len(data)):
+        print data[row]
 
-head(forNeo)
+ratings = []
+with open("/home/devinj/neo4jData/ydata-ymusic-rating-study-v1_0-test.txt",'rb') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter='\t')
+    for row in csvreader:
+        ratings.append(row)
+
+#takes a columns of [user, item, rating] and preps for neo node & relationship creation
+def prepForNeo(data):
+    from py2neo import neo4j, node, rel
+    users   = []
+    ratings = []
+    songs   = []
+    useridx = 0
+    songidx = 0
+    userdict = {}
+    songdict = {}
+
+    for row in data:
+        ratings.append(row)
+        if row[0] not in userdict:
+            users.append(node(uid=row[0]))
+            userdict[row[0]]=useridx
+            useridx += 1
+        if row[1] not in songdict:
+            songs.append(node(songid=row[1]))
+            songdict[row[1]]=songidx
+            songidx += 1
+
+    neoRatings = []
+    for rating in ratings:
+        neoRatings.append(rel(userdict[rating[0]],("RATED",{"rating":rating[2]}),songdict[rating[1]]))
+
+    allTogether = users + songs + neoRatings
+    
+    return users,songs,neoRatings,allTogether
+
+
+users,songs,neoRatings,allTogether = prepForNeo(ratings)
+
+
+
+
+
+
+
+
 
 graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-batch = neo4j.WriteBatch(graph_db)
 
-graph_db.create(rel(currentUser,("RATED",{"rating": 5}),currentSong))
-
-for row in range(0,1):
-    graph_db.create(forNeo[row])
-
+#batch = neo4j.WriteBatch(graph_db)
 #neoStuff =  batch.submit()
 
 
-
-#upload = graph_db.create(forNeo[0])
-
-
+#upload = graph_db.create(*allTogether)
 
 neo4j._add_header('X-Stream', 'true;format=pretty')
 
